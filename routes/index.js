e1dd27af1c1ad3a07f2eef71fe349d0e318320e2
haripatel123 <patelhari023@gmail.com>
2026-07-03 21:30:50 +0530

    Revert "Create interactive navbar-demo comparison sandbox to preview Vercel Dock, Linear Sidebar, and Command Nav styles"
    This reverts commit 02841c9a87bceaa79cc42d22245f5ee8d6e3595f.

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,4 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.index);
 
-// Navbar redesign options showcase playground
-router.get('/navbar-demo', (req, res) => {
-    res.render('navbar-demo', {
-        title: 'Navbar Redesign Sandbox',
-        currentUrl: '/navbar-demo',
-        currentCustomer: 1,
-        currentProvider: 1
-    });
-});
-
 module.exports = router;

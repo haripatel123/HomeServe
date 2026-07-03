02841c9a87bceaa79cc42d22245f5ee8d6e3595f
haripatel123 <patelhari023@gmail.com>
2026-07-03 21:28:27 +0530

    Create interactive navbar-demo comparison sandbox to preview Vercel Dock, Linear Sidebar, and Command Nav styles

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,14 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.index);
 
+// Navbar redesign options showcase playground
+router.get('/navbar-demo', (req, res) => {
+    res.render('navbar-demo', {
+        title: 'Navbar Redesign Sandbox',
+        currentUrl: '/navbar-demo',
+        currentCustomer: 1,
+        currentProvider: 1
+    });
+});
+
 module.exports = router;
